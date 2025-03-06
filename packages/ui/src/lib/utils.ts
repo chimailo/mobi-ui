8dfce21bf90437a6a291179d8aa51db077c1912c
Chima Ilo <collins.chima.ilo@gmail.com>
2025-03-06 05:10:14 +0100

    lint & format files

--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -1,25 +1,25 @@
-import { clsx, type ClassValue } from "clsx";
+import { type ClassValue, clsx } from "clsx";
 import { composeRenderProps } from "react-aria-components";
 import { twMerge } from "tailwind-merge";
 import { tv } from "tailwind-variants";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+	return twMerge(clsx(inputs));
 }
 
 export const focusRing = tv({
-  base: "focused:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-  variants: {
-    isFocusVisible: {
-      false: "outline-0",
-      true: "outline-2",
-    },
-  },
+	base: "focused:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+	variants: {
+		isFocusVisible: {
+			false: "outline-0",
+			true: "outline-2",
+		},
+	},
 });
 
 export function composeTailwindRenderProps<T>(
-  className: string | ((v: T) => string) | undefined,
-  tw: string
+	className: string | ((v: T) => string) | undefined,
+	tw: string,
 ): string | ((v: T) => string) {
-  return composeRenderProps(className, (className) => twMerge(tw, className));
+	return composeRenderProps(className, (className) => twMerge(tw, className));
 }
--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-  entryPoints: ["src/index.ts"],
-  format: ["esm"],
-  dts: true,
-  external: ["react"],
-  sourcemap: true, // generating sourcemaps
-  clean: true, // clean the output directory before each build
-  minify: true, // enable minification for smaller bundle size
-  treeshake: true, // remove unused code using rollup treeshake
-  ...options,
+	entryPoints: ["src/index.ts"],
+	format: ["esm"],
+	dts: true,
+	external: ["react"],
+	sourcemap: true, // generating sourcemaps
+	clean: true, // clean the output directory before each build
+	minify: true, // enable minification for smaller bundle size
+	treeshake: true, // remove unused code using rollup treeshake
+	...options,
 }));
